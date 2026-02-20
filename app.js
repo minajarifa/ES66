4fdbb453dcaf0ee6ad5042326678cc26b2d645f3
Your Name <minajmiha02@gmail.com>
2026-02-20 16:21:45 +0600

    added 3 funnction

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 // veriable
-let name = "arifa";
+// let name = "arifa";
 // console.log(name)
 //  name = "jaima"
-const frineds=['Arifa','Nabila','Jakiye','Jannat','Airin']
 // frineds.push("yeasmin")
 // frineds.pop()
 // frineds.unshift("Arifa")
 // frineds.shift()
-const sumfriends= frineds.slice(2,3)
-console.log(sumfriends);
+// const sumfriends= frineds.slice(2,3)
+// console.log("sumfriends",sumfriends)
+// console.log(frineds.splice(2,3));
+// for (let index = 0; index < frineds.length; index++) {
+//   const element = frineds[index];
+//   console.log(element);
+// }
+// function sum(a,b) {
+//   console.log(...arguments)
+//   const result = a+b;
+//   console.log(result)
+//   return result
+// }
+// const output= sum(2,10);
+// console.log(output)
+const frineds = ["Arifa", "Nabila", "Jakiye", "Jannat", "Airin"];
+const persome = {
+  name: "arifa",
+  age: 21,
+  frined: frineds,
+};
+persome.name= "Hero Alom"
+console.log(persome)
