e252d38a8b855f3dad69bd0a5bc70426babd5eb0
Your Name <minajmiha02@gmail.com>
2026-02-20 21:48:40 +0600

    find,filter,map and forEach practice

--- arraymethod.js
+++ arraymethod.js
@@ -1,23 +1,31 @@
+const products = [
+  { name: "redmi", brand: "xiaomi", price: 30000, color: "black" },
+  { name: "sumsung", brand: "xiaomi", price: 40000, color: "black" },
+  { name: "galaxy", brand: "xiaomi", price: 50000, color: "black" },
+  { name: "sumsung", brand: "xiaomi", price: 60000, color: "black" },
+  { name: "sumsung", brand: "xiaomi", price: 70000, color: "black" },
+  { name: "iphone", brand: "xiaomi", price: 80000, color: "black" },
+  { name: "oppo", brand: "xiaomi", price: 90000, color: "black" },
+  { name: "sumsung", brand: "xiaomi", price: 10000, color: "black" },
+  { name: "redmi", brand: "xiaomi", price: 20000, color: "black" },
+  { name: "sumsung", brand: "xiaomi", price: 30000, color: "black" },
+  { name: "oppo", brand: "xiaomi", price: 40000, color: "black" },
+  { name: "endroyed", brand: "xiaomi", price: 50000, color: "black" },
+  { name: "redmi", brand: "xiaomi", price: 60000, color: "black" },
+  { name: "redmi", brand: "xiaomi", price: 70000, color: "black" },
+  { name: "redmi", brand: "xiaomi", price: 80000, color: "black" },
+  { name: "redmi", brand: "xiaomi", price: 90000, color: "black" },
+  { name: "redmi", brand: "xiaomi", price: 30000, color: "black" },
+  { name: "redmi", brand: "xiaomi", price: 30000, color: "black" },
+  { name: "redmi", brand: "xiaomi", price: 30000, color: "black" },
+  { name: "redmi", brand: "xiaomi", price: 30000, color: "black" },
+];
 
-const products=[
-    {name:"redmi",brand:"xiaomi",price:30000,color:"black"},
-    {name:"sumsung",brand:"xiaomi",price:40000,color:"black"},
-    {name:"galaxy",brand:"xiaomi",price:50000,color:"black"},
-    {name:"sumsung",brand:"xiaomi",price:60000,color:"black"},
-    {name:"sumsung",brand:"xiaomi",price:70000,color:"black"},
-    {name:"iphone",brand:"xiaomi",price:80000,color:"black"},
-    {name:"oppo",brand:"xiaomi",price:90000,color:"black"},
-    {name:"sumsung",brand:"xiaomi",price:10000,color:"black"},
-    {name:"redmi",brand:"xiaomi",price:20000,color:"black"},
-    {name:"sumsung",brand:"xiaomi",price:30000,color:"black"},
-    {name:"oppo",brand:"xiaomi",price:40000,color:"black"},
-    {name:"endroyed",brand:"xiaomi",price:50000,color:"black"},
-    {name:"redmi",brand:"xiaomi",price:60000,color:"black"},
-    {name:"redmi",brand:"xiaomi",price:70000,color:"black"},
-    {name:"redmi",brand:"xiaomi",price:80000,color:"black"},
-    {name:"redmi",brand:"xiaomi",price:90000,color:"black"},
-    {name:"redmi",brand:"xiaomi",price:30000,color:"black"},
-    {name:"redmi",brand:"xiaomi",price:30000,color:"black"},
-    {name:"redmi",brand:"xiaomi",price:30000,color:"black"},
-    {name:"redmi",brand:"xiaomi",price:30000,color:"black"},
-]
+// const result = products.map((product) => product.length);
+// console.log(result);
+// products.forEach(product=>{console.log(product.price)})
+// const result = products.filter((product) => product.name === "redmi");
+// const result = products.filter((product) => product.price>=25000);
+// find 1 ta object return kore_________________
+const result = products.find(product=>product.name==="iphone")
+console.log(result);
