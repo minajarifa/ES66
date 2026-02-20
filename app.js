150a61558463ef12207ada97aa5a96b96b274f43
Your Name <minajmiha02@gmail.com>
2026-02-20 15:17:48 +0600

    start

---
+++ app.js
@@ -0,0 +1,11 @@
+// veriable
+let name = "arifa";
+// console.log(name)
+//  name = "jaima"
+const frineds=['Arifa','Nabila','Jakiye','Jannat','Airin']
+// frineds.push("yeasmin")
+// frineds.pop()
+// frineds.unshift("Arifa")
+// frineds.shift()
+const sumfriends= frineds.slice(2,3)
+console.log(sumfriends);
