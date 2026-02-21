49bc35f7f11e422a88e88baa537ae471e2f0aec9
Your Name <minajmiha02@gmail.com>
2026-02-21 21:28:53 +0600

    added shortcut.js

---
+++ shortcut.js
@@ -0,0 +1,11 @@
+// false value
+//  0 -0 "" null undefined
+// "asdfgh" []{}true
+const test ="rahim"
+// if (test) {
+//     console.log("It's Truty")
+// } else {
+//     console.log("It's falsy")
+// }
+// test? console.log("It's truty"):console.log("It's falsy")
+
