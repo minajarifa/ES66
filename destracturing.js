a20d96014c006177bfa5d4c3d6fd3e98b9c6efeb
Your Name <minajmiha02@gmail.com>
2026-02-21 20:18:20 +0600

    added fetch.js

--- destracturing.js
+++ destracturing.js
@@ -2,11 +2,11 @@
 // const [element1,element2,element3,element4,element5]=friends
 // console.log(element1)
 
-const person = {
-  name: "redmi",
-  brand: "xiaomi",
-  price: 30000,
-  color: "black",
-};
-const { name, brand, price, color } = person;
-console.log(brand)
+// const person = {
+//   name: "redmi",
+//   brand: "xiaomi",
+//   price: 30000,
+//   color: "black",
+// };
+// const { name, brand, price, color } = person;
+// console.log(brand)
---
+++ fetch.js
@@ -0,0 +1,49 @@
+// JSON
+// const person = {
+//   name: "arifa",
+//   age: 21,
+//   friends: ["Irin", "Aysha", "Nabila", "Farhana", "Jakiya", "Mim"],
+//   family: {
+//     fatherName: "Momin",
+//     motherName: "Asiya",
+//   },
+// };
+// const jsonData = JSON.stringify(person);
+// const plantDate = JSON.parse(jsonData);
+// console.log(plantDate);
+
+// fetch
+// fetch(`....`)
+//   .then((res) => res.json())
+//   .then((data) => {
+//     console.log(data);
+//   });
+
+const person = {
+  name: "arifa",
+  age: 21,
+  friends: ["Irin", "Aysha", "Nabila", "Farhana", "Jakiya", "Mim"],
+  family: {
+    fatherName: "Momin",
+    motherName: "Asiya",
+  },
+};
+// const key = Object.keys(person);
+// const key = Object.values(person);
+// console.log(key)
+
+const products = [
+  { name: "redmi", brand: "xiaomi", price: 30000, color: "black" },
+  { name: "sumsung", brand: "xiaomi", price: 40000, color: "black" },
+  { name: "galaxy", brand: "xiaomi", price: 50000, color: "black" },
+  { name: "sumsung", brand: "xiaomi", price: 60000, color: "black" },
+  { name: "sumsung", brand: "xiaomi", price: 70000, color: "black" },
+  { name: "iphone", brand: "xiaomi", price: 80000, color: "black" },
+  { name: "oppo", brand: "xiaomi", price: 90000, color: "black" },
+  { name: "sumsung", brand: "xiaomi", price: 10000, color: "black" },
+  { name: "redmi", brand: "xiaomi", price: 20000, color: "black" },
+  { name: "sumsung", brand: "xiaomi", price: 30000, color: "black" },
+  { name: "oppo", brand: "xiaomi", price: 40000, color: "black" },
+  { name: "endroyed", brand: "xiaomi", price: 50000, color: "black" },
+];
+
